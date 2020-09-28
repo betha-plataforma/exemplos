59a95780ac8caf1ef5096b7131cb309654c04414
Cristiano Soares <criscoder@msn.com>
2020-09-28 17:01:11 -0300

    primeiro commit

---
+++ theme-bootstrap-4-angular/src/app/menu1/menu1.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu1',
+  templateUrl: './menu1.component.html',
+})
+export class Menu1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ theme-bootstrap-4-angular/src/app/menu2/menu2.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu2',
+  templateUrl: './menu2.component.html',
+})
+export class Menu2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
