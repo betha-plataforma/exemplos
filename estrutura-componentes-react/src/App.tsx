5bc65eaf8520bfd8ee606fd61df5a90c36bc3a78
Cristiano Soares <criscoder@msn.com>
2020-12-17 08:40:21 -0300

    Adiciona react-router

--- estrutura-componentes-react/src/App.tsx
+++ estrutura-componentes-react/src/App.tsx
@@ -1,36 +1,59 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useMemo } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import BthMarcaProdutoWrapper from './estrutura-wrappers/BthMarcaProdutoWrapper';
 import MainComponent from './MainComponent';
 
 const App: React.FC<{}> = ({ }) => {
   const elementRef = useRef(null);
-  const opcoesMenu = [
-    { id: 'menu1', descricao: 'Menu 1', icone: 'chart-pie', rota: '/', possuiPermissao: true },
+  const opcoesMenu = useMemo(() => [
     {
-      id: '', descricao: 'Menu 2', icone: 'cog', possuiPermissao: true,
-      submenus: [
-        { id: 'menu2', descricao: 'Submenu 1', rota: '/menu2', possuiPermissao: true },
-        { id: 'menu3', descricao: 'Submenu 2', rota: '/menu3', possuiPermissao: true },
-      ]
+      id: 'menu1', descricao: 'Menu 1', rota: '/', possuiPermissao: true
     },
-  ];
+    {
+      id: 'menu2', descricao: 'Menu 2', rota: '/teste', possuiPermissao: true
+    },
+  ], []);
+
+  // componentDidUpdate(prevProps) {
+  //   if (this.props.location !== prevProps.location) {
+  //     this.onRouteChanged();
+  //   }
+  // }
+
+  // onRouteChanged() {
+  //   console.log("ROUTE CHANGED");
+  // }
   useEffect(() => {
     const element = (elementRef.current as any)!;
     element.opcoes = opcoesMenu;
     element.menuVertical = false;
+    // element.setMenuAtivo('menu2');
+    // window.location.replace('/teste')
+    element.addEventListener('opcaoMenuSelecionada', (event: CustomEvent<any>) => {
+      console.log('menu selecionado', event)
+      element.setMenuAtivo(event.detail.id);
+      // window.location.href = event.detail.rota;
+    });
   }, [opcoesMenu]);
 
-  return (<bth-app ref={elementRef}>
-    <BthMarcaProdutoWrapper produto="React" exibirProdutos={true}></BthMarcaProdutoWrapper>
-    <bth-utilitarios slot="menu_ferramentas"></bth-utilitarios>
-    <bth-ajuda slot="menu_ferramentas"></bth-ajuda>
-    <bth-novidades slot="menu_ferramentas"></bth-novidades>
-    <bth-notificacoes slot="menu_ferramentas"></bth-notificacoes>
-    <main slot="container_aplicacao">
-      <MainComponent></MainComponent>
-    </main>
-  </bth-app>);
+  return (
+    <Router>
+      <Switch>
+        <bth-app ref={elementRef}>
+          <BthMarcaProdutoWrapper produto="React" exibirProdutos={true}></BthMarcaProdutoWrapper>
+          <bth-utilitarios slot="menu_ferramentas"></bth-utilitarios>
+          <bth-ajuda slot="menu_ferramentas"></bth-ajuda>
+          <bth-novidades slot="menu_ferramentas"></bth-novidades>
+          <bth-notificacoes slot="menu_ferramentas"></bth-notificacoes>
+          <main slot="container_aplicacao" className="container-fluid mt-2">
+            <Route exact path="/"></Route>
+            <Route exact path="/teste" component={MainComponent} ></Route>
+          </main>
+        </bth-app>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
